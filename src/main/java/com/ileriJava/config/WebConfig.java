0270622725ddc8db61301559f429533cbdebc3d7
alperenyozcu <79807548+alperenyozcu@users.noreply.github.com>
2022-03-24 21:22:20 +0300

    Yapılan Değişiklikler: Database için config denemeleri yapıldı entity denemeleri yapıldı fakat hibernate ile otomatik veritabanına tablo oluşturamadım.

--- src/main/java/com/ileriJava/config/WebConfig.java
+++ src/main/java/com/ileriJava/config/WebConfig.java
@@ -1,10 +1,13 @@
-package com.ileriJava.config;
 
+package com.ileriJava.config;
+import com.sun.xml.bind.v2.TODO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -12,6 +15,12 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.ileriJava")
 public class WebConfig {
+
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+    //WEB-INF klasörü view klasörü altında aranmalı
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
